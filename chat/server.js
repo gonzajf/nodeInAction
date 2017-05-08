c354215425ce26712fa692936faf840c1f61a833
Gonza Franchino <gfranchino@conicet.gov.ar>
2017-05-08 15:54:55 -0300

    capitulo 2 - chat terminado

--- chat/server.js
+++ chat/server.js
@@ -4,28 +4,28 @@ var path = require('path');
 var mime = require('mime');
 var cache = {};
 
-
 function send404(response) {
     response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('Error 404: resource not found');
+    response.write('Error 404: resource not found.');
     response.end();
-
 }
 
-function  sendFile(response, filePath, fileContents) {
-    response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(
+        200,
+        {"content-type": mime.lookup(path.basename(filePath))}
+    );
     response.end(fileContents);
 }
 
-function serveStatic(response, cache, absPath){
-
-    if(cache[absPath]){
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
         sendFile(response, absPath, cache[absPath]);
     } else {
-        fs.exists(absPath, function (exists) {
-            if(exists){
-                fs.readFile(absPath, function (err, data) {
-                    if(err){
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
                         send404(response);
                     } else {
                         cache[absPath] = data;
@@ -41,18 +41,19 @@ function serveStatic(response, cache, absPath){
 
 var server = http.createServer(function(request, response) {
     var filePath = false;
-
-    if(request.url == '/') {
+    if (request.url == '/') {
         filePath = 'public/index.html';
     } else {
         filePath = 'public' + request.url;
     }
 
     var absPath = './' + filePath;
-    serveStatic(response, request, absPath);
+    serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function () {
-    console.log('server listening on port 3000');
-})
+server.listen(3000, function() {
+    console.log("Server listening on port 3000.");
+});
 
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
